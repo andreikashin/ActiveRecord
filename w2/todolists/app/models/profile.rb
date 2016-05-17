b04cd6a4b3e0a530a156eb8ac4365af19e034be2
andreikashin <andreikashin@gmail.com>
2016-05-18 02:49:05 +0300

    Homework 1-11

--- w2/todolists/app/models/profile.rb
+++ w2/todolists/app/models/profile.rb
@@ -1,3 +1,18 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
+  
+    
+  validate :first_and_last_not_null
+  validates :gender, format: {with: /male|female/}
+  validate :not_male_sue
+
+  def first_and_last_not_null()
+    if not first_name and not last_name
+      errors.add(:first_name, "to be not null")
+    end
+  end
+
+  def not_male_sue
+    errors.add(:gender, "can't be male") if gender == "male" and first_name == "Sue"
+  end
 end
