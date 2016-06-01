ee086d8e6cfd719246f2c0b960c91e751a4de545
andreikashin <andreikashin@gmail.com>
2016-06-02 01:55:09 +0300

    book note app

---
+++ w4/i_reviewed/db/seeds.rb
@@ -0,0 +1,17 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Book.destroy_all
+
+Book.create! [
+  { name: "Eloquent RUby", author: "A" },
+  { name: "Beginning ruby", author: "B" },
+  { name: "Metaprog ruby", author: "C" },
+  { name: "Design paterns", author: "D" },
+  { name: "The ruby prog lang", author: "E" }
+]
