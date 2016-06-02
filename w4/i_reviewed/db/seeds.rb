cd14b4bcdd980e9e7d433fb2bde9153dcb473904
andreikashin <andreikashin@gmail.com>
2016-06-03 01:29:57 +0300

    w4 examples

--- w4/i_reviewed/db/seeds.rb
+++ w4/i_reviewed/db/seeds.rb
@@ -7,11 +7,28 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Book.destroy_all
+Reviewer.destroy_all
 
 Book.create! [
-  { name: "Eloquent RUby", author: "A" },
-  { name: "Beginning ruby", author: "B" },
-  { name: "Metaprog ruby", author: "C" },
+  { name: "Eloquent Ruby", author: "A" },
+  { name: "Beginning Ruby", author: "B" },
+  { name: "Metaprog Ruby", author: "C" },
   { name: "Design paterns", author: "D" },
   { name: "The ruby prog lang", author: "E" }
 ]
+
+eloquent = Book.find_by name: "Eloquent Ruby"
+eloquent.notes.create! [
+  { title: "wow", note: "Great book WOW" },
+  { title: "woohoo", note: "Great book woohoo" }
+]
+
+reviewers = Reviewer.create! [
+  { name: "Joe", password: "abc123" },
+  { name: "Jim", password: "123abc" }
+]
+
+Book.all.each do |book|
+  book.reviewer = reviewers.sample
+  book.save!
+end
