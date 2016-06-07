3bff23c3d8b715211427fc560547df7a10cad114
andreikashin <andreikashin@gmail.com>
2016-06-08 01:48:16 +0300

    w4 examples authorize

--- w4/i_reviewed/app/controllers/sessions_controller.rb
+++ w4/i_reviewed/app/controllers/sessions_controller.rb
@@ -1,47 +1,22 @@
 class SessionsController < ApplicationController
+  skip_before_action :ensure_login, only: [:new, :create]
   def new
-    # Login page - new.html.erb
   end
-  
+
   def create
-    reviewer = Reviewer.find_by(:name params[:reviewer][:name])
+    reviewer = Reviewer.find_by(name: params[:reviewer][:name])
     password = params[:reviewer][:password]
     
     if reviewer && reviewer.authenticate(password)
-      sessions[:reviewer_id] = reviewer.id
+      session[:reviewer_id] = reviewer.id
       redirect_to root_path, notice: "Login success"
     else
       redirect_to login_path, alert: "Invalid login"
     end
   end
-  
+
   def destroy
     reset_session
     redirect_to login_path, notice: "Logged out"
   end
-end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 end
